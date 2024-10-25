030a7c9cb00bd4e15a90bf2ff189fe97cde33c0f
rulo-dev <rulosan77@gmail.com>
2024-10-24 23:21:32 -0600

    añadiendo componentes

---
+++ src/components/Body/body.jsx
@@ -0,0 +1,12 @@
+//en este componente guardaremos el contenido principal de la app
+//NOTA: en todos los componentes usaremos snippets aunque ya los usabamos
+// el snippet que usaremos es el: rafce
+import React from 'react';
+
+const Body = () => {
+  return <div>BODY</div>;
+};
+
+export default Body;
+
+//ctrl + shihf + l = seleccionar varios textos
